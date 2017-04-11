adc48e719d064a2157bdb91781afc4323167a18f
Tom White <tom@cloudera.com>
2017-04-11 09:53:00 +0100

    Refactoring

--- q4_spark_eval/test_case_md.sh
+++ q4_spark_eval/test_case_md.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env bash
 
-time_gatk() {
-  GATK_ARGS=$1
-  NUM_EXECUTORS=$2
-  EXECUTOR_CORES=$3
-  EXECUTOR_MEMORY=$4
-  DRIVER_MEMORY=$5
-  COMMAND=$(echo $GATK_ARGS | awk '{print $1}')
-  LOG=${COMMAND}_$(date +%Y%m%d_%H%M%S).log
-  ./gatk-launch $GATK_ARGS \
-    -- \
-    --sparkRunner SPARK --sparkMaster yarn-client --sparkSubmitCommand spark2-submit \
-    --num-executors $NUM_EXECUTORS --executor-cores $EXECUTOR_CORES --executor-memory $EXECUTOR_MEMORY \
-    --driver-memory $DRIVER_MEMORY \
-  > $LOG 2>&1
-  RC=$?
-  DURATION_MINS=$(grep 'Elapsed time' $LOG | grep -Eow "[0-9]+\.[0-9][0-9]")
-  echo "$GATK_ARGS,$NUM_EXECUTORS,$EXECUTOR_CORES,$EXECUTOR_MEMORY,$RC,$DURATION_MINS"
-}
+. utils.sh
+
+RESULTS_CSV=test_case_md_results.csv
 
 for num_exec in 64
 do
-  time_gatk "MarkDuplicatesSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded --shardedOutput true" $num_exec 1 4g 4g >> test_case_md_results.csv
+  time_gatk "MarkDuplicatesSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded --shardedOutput true" $num_exec 1 4g 4g >> $RESULTS_CSV
   time_gatk "SortReadFileSpark -I hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded -O hdfs:///user/$USER/q4_spark_eval/out/markdups" $num_exec 1 4g 4g
 done
---
+++ q4_spark_eval/utils.sh
@@ -0,0 +1,16 @@
+time_gatk() {
+  GATK_ARGS=$1
+  NUM_EXECUTORS=$2
+  EXECUTOR_CORES=$3
+  EXECUTOR_MEMORY=$4
+  COMMAND=$(echo $GATK_ARGS | awk '{print $1}')
+  LOG=${COMMAND}_$(date +%Y%m%d_%H%M%S).log
+  ./gatk-launch $GATK_ARGS \
+    -- \
+    --sparkRunner SPARK --sparkMaster yarn-client --sparkSubmitCommand spark2-submit \
+    --num-executors $NUM_EXECUTORS --executor-cores $EXECUTOR_CORES --executor-memory $EXECUTOR_MEMORY \
+  > $LOG 2>&1
+  RC=$?
+  DURATION_MINS=$(grep 'Elapsed time' $LOG | grep -Eow "[0-9]+\.[0-9][0-9]")
+  echo "$GATK_ARGS,$NUM_EXECUTORS,$EXECUTOR_CORES,$EXECUTOR_MEMORY,$RC,$DURATION_MINS"
+}
