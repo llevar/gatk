ba66906f9ba7296ab229afb9e34f2b75ea20d20e
Tom White <tom@cloudera.com>
2017-04-11 10:25:04 +0100

    Add run_id, as yet unused, to group tests with more than one command.

--- q4_spark_eval/test_case_2.sh
+++ q4_spark_eval/test_case_2.sh
@@ -6,9 +6,11 @@ RESULTS_CSV=test_case_2_results.csv
 
 echo 'Command,Number of executors,Executor cores,Executor memory,Exit code,Time (mins)' > $RESULTS_CSV
 
+run_id=0
 for exec_cores in 1 2 4; do
   for total_cores in 4 8 16 32 64; do
     num_exec=$((total_cores * exec_cores))
     time_gatk "CountReadsSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam" $num_exec $exec_cores 1g 1g >> $RESULTS_CSV
+    run_id=$((run_id+1))
   done
 done
--- q4_spark_eval/test_case_md.sh
+++ q4_spark_eval/test_case_md.sh
@@ -4,10 +4,12 @@
 
 RESULTS_CSV=test_case_md_results.csv
 
+run_id=0
 for exec_cores in 1; do
   for total_cores in 64; do
     num_exec=$((total_cores * exec_cores))
     time_gatk "MarkDuplicatesSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded --shardedOutput true" $num_exec $exec_cores 4g 4g >> $RESULTS_CSV
     time_gatk "SortReadFileSpark -I hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded -O hdfs:///user/$USER/q4_spark_eval/out/markdups" $num_exec $exec_cores 4g 4g
+    run_id=$((run_id+1))
   done
 done
