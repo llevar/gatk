85759999ffe1155ba8d05c671d9b0908a6ec1d02
Tom White <tom@cloudera.com>
2017-04-11 10:17:47 +0100

    Fix

--- q4_spark_eval/test_case_4.sh
+++ q4_spark_eval/test_case_4.sh
@@ -9,6 +9,6 @@ echo 'Command,Number of executors,Executor cores,Executor memory,Exit code,Time 
 for exec_cores in 1 2 4; do
   for total_cores in 4 8 16 32 64; do
     num_exec=$((total_cores * exec_cores))
-    time_gatk "CountReadsSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -L Broad.human.exome.b37.interval_list" $num_exec $exec_cores 1g >> $RESULTS_CSV
+    time_gatk "CountReadsSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -L Broad.human.exome.b37.interval_list" $num_exec $exec_cores 1g 1g >> $RESULTS_CSV
   done
 done
--- q4_spark_eval/utils.sh
+++ q4_spark_eval/utils.sh
@@ -3,12 +3,14 @@ time_gatk() {
   NUM_EXECUTORS=$2
   EXECUTOR_CORES=$3
   EXECUTOR_MEMORY=$4
+  DRIVER_MEMORY=$5
   COMMAND=$(echo $GATK_ARGS | awk '{print $1}')
   LOG=${COMMAND}_$(date +%Y%m%d_%H%M%S).log
   ./gatk-launch $GATK_ARGS \
     -- \
     --sparkRunner SPARK --sparkMaster yarn-client --sparkSubmitCommand spark2-submit \
     --num-executors $NUM_EXECUTORS --executor-cores $EXECUTOR_CORES --executor-memory $EXECUTOR_MEMORY \
+    --driver-memory $DRIVER_MEMORY \
   > $LOG 2>&1
   RC=$?
   DURATION_MINS=$(grep 'Elapsed time' $LOG | grep -Eow "[0-9]+\.[0-9][0-9]")
