e4bc6e0f437372137569ea0330c6c650297b53e8
Tom White <tom@cloudera.com>
2017-04-19 11:02:36 +0100

    Go back to overlaps partitioner with lots of cores per executor in hope that broadcasting 10gb of variants is OK

--- q4_spark_eval/test_case_pipeline.sh
+++ q4_spark_eval/test_case_pipeline.sh
@@ -5,11 +5,11 @@
 RESULTS_CSV=test_case_pipeline_results.csv
 
 run_id=0
-for exec_cores in 1; do
-  for total_cores in 64; do
-    num_exec=$((total_cores * exec_cores))
+for exec_cores in 8; do
+  for total_cores in 32; do
+    num_exec=$((total_cores / exec_cores))
     #time_gatk "MarkDuplicatesSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded --shardedOutput true" $num_exec $exec_cores 4g 4g >> $RESULTS_CSV
-    time_gatk "BQSRPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded -O hdfs:///user/$USER/q4_spark_eval/out/bqsr-sharded --shardedOutput true -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs:///user/$USER/q4_spark_eval/dbsnp_138.b37.vcf --joinStrategy SHUFFLE" $num_exec $exec_cores 6g 4g >> $RESULTS_CSV
+    time_gatk "BQSRPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded -O hdfs:///user/$USER/q4_spark_eval/out/bqsr-sharded --shardedOutput true -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs:///user/$USER/q4_spark_eval/dbsnp_138.b37.vcf --joinStrategy OVERLAPS_PARTITIONER" $num_exec $exec_cores 6g 32g >> $RESULTS_CSV
     #time_gatk "ReadsPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/reads-sharded --shardedOutput true -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs:///user/$USER/q4_spark_eval/dbsnp_138.vcf" $num_exec $exec_cores 4g 4g >> $RESULTS_CSV
     run_id=$((run_id+1))
   done
