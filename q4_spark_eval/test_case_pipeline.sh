83599ca5514f93897817b1eb59e433f65e142166
Tom White <tom@cloudera.com>
2017-04-19 16:41:08 +0100

    Try a small 6GB BAM to start with for BQSR

--- q4_spark_eval/test_case_pipeline.sh
+++ q4_spark_eval/test_case_pipeline.sh
@@ -9,7 +9,8 @@ for exec_cores in 8; do
   for total_cores in 32; do
     num_exec=$((total_cores / exec_cores))
     #time_gatk "MarkDuplicatesSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded --shardedOutput true" $num_exec $exec_cores 4g 4g >> $RESULTS_CSV
-    time_gatk "BQSRPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded -O hdfs:///user/$USER/q4_spark_eval/out/bqsr-sharded --shardedOutput true -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs:///user/$USER/q4_spark_eval/dbsnp_138.b37.vcf --joinStrategy OVERLAPS_PARTITIONER" $num_exec $exec_cores 6g 32g >> $RESULTS_CSV
+    #time_gatk "BQSRPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/out/markdups-sharded -O hdfs:///user/$USER/q4_spark_eval/out/bqsr-sharded --shardedOutput true -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs:///user/$USER/q4_spark_eval/dbsnp_138.b37.vcf --joinStrategy OVERLAPS_PARTITIONER" $num_exec $exec_cores 32g 16g >> $RESULTS_CSV
+    time_gatk "BQSRPipelineSpark -I bam/CEUTrio.HiSeq.WGS.b37.ch1.1m-65m.NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/bqsr-sharded --shardedOutput true -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs:///user/$USER/q4_spark_eval/dbsnp_138.b37.vcf --joinStrategy OVERLAPS_PARTITIONER" $num_exec $exec_cores 32g 16g >> $RESULTS_CSV
     #time_gatk "ReadsPipelineSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/reads-sharded --shardedOutput true -R hdfs:///user/$USER/q4_spark_eval/human_g1k_v37.2bit --knownSites hdfs:///user/$USER/q4_spark_eval/dbsnp_138.vcf" $num_exec $exec_cores 4g 4g >> $RESULTS_CSV
     run_id=$((run_id+1))
   done
